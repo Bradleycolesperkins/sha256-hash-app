1c26bcaf5d2d9341d263a8c533b86fcfd8578f9c
Bradley Coles-Perkins <bradleycolesperkins@gmail.com>
2025-11-14 10:49:41 +0000

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ function App() {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-3xl font-bold">SHA256 Hash App</h1>
-    </div>
+        <h1 className="text-3xl font-bold">SHA256 Hash App</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
